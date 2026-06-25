c8b3453d9c405096aaca6111ca608fd8c9c9021e
Harooon1212 <135606132+Harooon1212@users.noreply.github.com>
2026-06-26 03:30:12 +0500

    Time: 4 ms (42.25%), Space: 23.9 MB (5.01%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,35 +10,48 @@
  */
 class Solution {
 public:
-ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* result=new ListNode(0);
-        ListNode* temp=result;
-        while(list1!=nullptr && list2!=nullptr){
-            if(list1->val < list2-> val){
-                temp->next=list1;
-                list1=list1->next;
+    // Merge two sorted linked lists
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
             }
-            else{
-                temp->next=list2;
-                list2=list2->next;
-            }
-            temp=temp->next;
+            temp = temp->next;
         }
-        if(list1!=nullptr) temp->next=list1;
-        if(list2!=nullptr) temp->next=list2;
+        if (list1 != nullptr)
+            temp->next = list1;
+        if (list2 != nullptr)
+            temp->next = list2;
+        return dummy->next;
+    }
 
-        return result->next;
+    // Divide and Conquer function
+    ListNode* merge(vector<ListNode*>& lists, int left, int right) {
+        // Base case: only one list
+        if (left == right)
+            return lists[left];
+
+        // Divide
+        int mid = left + (right - left) / 2;
+
+        // Conquer
+        ListNode* leftList = merge(lists, left, mid);
+        ListNode* rightList = merge(lists, mid + 1, right);
+
+        // Combine
+        return mergeTwoLists(leftList, rightList);
     }
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.empty())
             return nullptr;
-        int k=lists.size();
-        int count=0;
-        ListNode* temp=lists[0];
-        for(int i=1;i<k;i++){
-            temp=mergeTwoLists(temp,lists[i]);
-        }
-        return temp;      
+
+        return merge(lists, 0, lists.size() - 1);
     }
 };
